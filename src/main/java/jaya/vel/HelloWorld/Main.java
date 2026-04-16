d83e4cc8c8fc617d0e9834c88c2cec022748eeff
Jayavel2005 <jayavel1520@gmail.com>
2026-04-16 18:18:48 +0530

    practice: todo crud api

--- src/main/java/jaya/vel/HelloWorld/Main.java
+++
@@ -1,8 +0,0 @@
-package jaya.vel.HelloWorld;
-
-public class Main {
-    public static void main(String[] args) {
-        TodoService todoService = new TodoService();
-        todoService.printTodos();
-    }
-}
